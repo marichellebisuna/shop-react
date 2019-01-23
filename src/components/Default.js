3f744f60234f196d706c67e8892dacb26d624d40
Marichelle <marichellebisuna@yahoo.com>
2019-01-23 11:33:37 +1100

    created components

---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class h2 extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Page Not found</h3>
+            </div>
+        )
+    }
+}
